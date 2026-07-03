0a9ddf52031983cdbd2c708b612931152d9274f8
Emilieny Silva <emilieny48@gmail.com>
2026-07-03 14:06:42 -0300

    fist commit

---
+++ rtl/vending_pkg.sv
@@ -0,0 +1,11 @@
+package vending_pkg;
+	typedef enum logic [2:0] {
+	  IDLE = 3'b000,
+	  COLLECT = 3'b001,
+	  CHECK = 3'B010,
+	  DISPENSE = 3'b011,
+	  CHANGE = 3'b100,
+	  ERROR = 3'b101
+	} state_t;
+
+endpackage : veding_pkg
