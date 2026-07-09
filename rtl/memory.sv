b81402a18e38178bfeae5169261cd04914b5e846
Victor Jose <victorjcmedeiros@gmail.com>
2026-07-08 22:56:04 -0300

    update: update memory.sv

--- rtl/memory.sv
+++ rtl/memory.sv
@@ -14,12 +14,7 @@ module memory (
   // Formato: [15:8] = preço, [7:0] = estoque
   logic [15:0] mem_array [0:3];
 
-  initial begin
-    mem_array[0] = {8'd25,  8'd5};  // Item 0 (Café): R$ 0,25 (25 centavos), 5 no estoque
-    mem_array[1] = {8'd50,  8'd5};  // Item 1 (Água): R$ 0,50 (50 centavos), 5 no estoque
-    mem_array[2] = {8'd75,  8'd3};  // Item 2 (Suco): R$ 0,75 (75 centavos), 3 no estoque
-    mem_array[3] = {8'd100, 8'd2};  // Item 3 (Snack): R$ 1,00 (100 centavos), 2 no estoque
-  end
+
 
   always_ff @(posedge clk) begin
     if (rst) begin
