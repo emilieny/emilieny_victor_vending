78c9444d5e00405ee55754b9862a4d6a0ff35187
Victor Jose <victorjcmedeiros@gmail.com>
2026-07-09 19:33:04 -0300

    add: add comments

--- rtl/vending_top.sv
+++ rtl/vending_top.sv
@@ -19,7 +19,7 @@ module vending_top
   output logic [7:0] change,
   output logic       error,
   output logic [7:0] display,
-  output logic [2:0] state_out
+  output logic [2:0] 
 );
 
   // Fios internos de interconexão (Datapath <-> Control)
